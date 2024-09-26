7897b997f6e0afb9883bbd97767d201e368eaef0
michael <michael.samiley@gmail.com>
2024-09-25 21:36:49 -0700

    Updates

---
+++ assets/infoView-a3f45dde.js
@@ -0,0 +1,1 @@
+import"./index-00434a2a.js";import{o as e,c as t}from"./index-7e03e1e6.js";const o={class:"flex w-full justify-center"},a={__name:"infoView",setup(c){return(s,n)=>(e(),t("div",o," info "))}};export{a as default};
--- assets/infoView-b4a4dc3e.js
+++
@@ -1,1 +0,0 @@
-import"./index-00434a2a.js";import{o as e,c as t}from"./index-c4dd25a2.js";const o={class:"flex w-full justify-center"},a={__name:"infoView",setup(c){return(s,n)=>(e(),t("div",o," info "))}};export{a as default};
